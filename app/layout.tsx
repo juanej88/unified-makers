aec8b520d0607dfae3ea7e52aac7aace3dd9ba9f
Juan Espinosa <arq.jorrin@gmail.com>
2025-01-01 21:41:01 +0800

    Add favicon and basic metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,11 @@ import './globals.css';
 import { poppins } from './ui/fonts';
 
 export const metadata: Metadata = {
-  title: 'Unified Makers',
-  description: 'Unified Makers',
+  title: {
+    template: '%s | Unified Makers',
+    default: 'Unified Makers',
+  },
+  description: 'Unified Makers is a trusted platform connecting users with skilled professionals across various industries. Designed to simplify the search for reliable services, it empowers users to find and filter experts by category, location, and more, serving as a one-stop solution for all professional needs.',
 };
 
 export default function RootLayout({
