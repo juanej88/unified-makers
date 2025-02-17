2c3994991c2b04a8718e9fe24a7eec9bb97d167a
Juan Espinosa <arq.jorrin@gmail.com>
2025-02-17 23:23:52 +0800

    Show City and All when a city is selected, but not a category

--- app/ui/SearchButton.tsx
+++ app/ui/SearchButton.tsx
@@ -2,7 +2,6 @@
 
 import { useCity } from '@/app/providers/cityProvider';
 import { menu } from '@/app/lib/definitions';
-// import { Library } from 'lucide-react';
 
 function SearchButton({ toggleMenu }: { toggleMenu: (menu: menu) => void }) {
   const city = useCity();
@@ -10,9 +9,8 @@ function SearchButton({ toggleMenu }: { toggleMenu: (menu: menu) => void }) {
   return (
     <section className='flex-center'>
       <button id='location-btn' onClick={() => toggleMenu('location')} className='flex-center primary-btn px-3 py-1 text-secondary'>
-        {/* <Library size={18} strokeWidth={1.5} /> */}
-        <p className='truncate max-w-40'>
-          {city ? city : 'Category | Location'}
+        <p className='truncate max-w-40 pt-[2px]'>
+          {city ? `${city} | All` : 'Search'}
         </p>
       </button>
     </section>
